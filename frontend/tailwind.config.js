036ca4d7f493a7e9221cda2f92331544d5574acc
tianqiu <1421582341@qq.com>
2023-01-06 09:21:27 +0800

    style: change style

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx,html}', './node_modules/tw-elements/dist/js/**/*.js'],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx,html}',
+    './node_modules/tw-elements/dist/js/**/*.js'
+  ],
   theme: {
     extend: {
       lineHeight: {
@@ -9,16 +13,4 @@ module.exports = {
     }
   },
   plugins: [require('@tailwindcss/forms'), require('tw-elements/dist/plugin')]
-}
-
-// module.exports = {
-//   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-//   darkMode: false, // or 'media' or 'class'
-//   theme: {
-//     extend: {}
-//   },
-//   variants: {
-//     extend: {}
-//   },
-//   plugins: []
-// }
+};
