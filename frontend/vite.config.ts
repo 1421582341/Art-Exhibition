a202b743504eff06a1e1fec77ac13fc06bf98852
tianqiu <1421582341@qq.com>
2023-01-06 09:31:14 +0800

    feat: change vite&ts config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,46 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import * as path from 'path';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
-    vue()]
-})
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+      dts: 'src/auto-import.d.ts'
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass'
+        })
+      ],
+      dts: 'src/components.d.ts'
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, './src')
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'static/js/[name]-[hash].js',
+        entryFileNames: 'static/js/[name]-[hash].js',
+        assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+      }
+    },
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
+});
